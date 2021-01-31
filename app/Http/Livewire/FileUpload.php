ab4c21e5f9f6e9073fc47e6209a848a02e224d03
harveyayaay <harveyayaay@gmail.com>
2021-01-31 09:04:11 +0800

    Test

--- app/Http/Livewire/FileUpload.php
+++ app/Http/Livewire/FileUpload.php
@@ -33,7 +33,6 @@ class FileUpload extends Component
 
     public function save()
     {      
-      dd('sdfksdjf');
         // $this->validate([
         //     'photo' => 'image|max:1024', // 1MB Max
         // ]);
